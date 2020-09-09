2c12ca0937dbb502de9b0105f551c1085d46bdb1
ryohorie <develop@ryohorie.com>
2020-09-09 12:41:03 +0900

    READMEを追加

--- 2/main.js
+++ 2/main.js
@@ -1,1 +1,15 @@
 const data = require("./kana.json");
+
+function charToArray(ch) {
+  // 1文字chを12x10の2次元配列にして返す関数
+  // ※座標が存在している箇所にはアスタリスク*、ない位置にはスペースを格納してください
+}
+function printArray(arr) {
+  // 二次元配列をconsole.logする関数
+}
+
+const str = process.argv[2];
+for (let i = 0; i < str.length; i++) {
+  const arr = charToArray(str[i]);
+  printArray(arr);
+}
